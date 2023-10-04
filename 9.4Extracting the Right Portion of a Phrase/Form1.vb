7b6699806dbbad33ad1654304bd0511be9f09779
Lenovo-User <Lenovo-User@DESKTOP-RNPFAS5>
2023-10-04 18:58:56 +0200

    Add project files.

---
+++ 9.4Extracting the Right Portion of a Phrase/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+	Private Sub Button1_Click(sender As Object, e As EventArgs) Handles BtnShow.Click
+		Dim MyText As String
+		MyText = "Visual Basic"
+		MsgBox(Microsoft.VisualBasic.Right(MyText, 8))
+
+	End Sub
+End Class
